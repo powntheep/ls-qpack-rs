aab0d3c735f703f16601b63cbc6a1fb13ec51e3f
sb9k <132234640+sb9k@users.noreply.github.com>
2023-11-15 23:44:17 +0700

    feat: added android build config

--- ls-qpack-sys/build.rs
+++ ls-qpack-sys/build.rs
@@ -3,13 +3,42 @@
 use std::env;
 use std::path::PathBuf;
 
+fn get_abi(target: String) -> String {
+    if target.contains("armv7") {
+        "armeabi-v7a".to_string()
+    } else if target.contains("aarch64") {
+        "arm64-v8a".to_string()
+    } else if target.contains("i686") {
+        "x86".to_string()
+    } else if target.contains("x86_64") {
+        "x86_64".to_string()
+    } else {
+        panic!("Unknown target {}", target);
+    }
+}
+
 fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     let ls_qpack_dep_dir = PathBuf::from("deps/ls-qpack");
 
-    cmake::Config::new(&ls_qpack_dep_dir)
-        .define("LSQPACK_BIN", "OFF")
-        .build();
+    let target = env::var("TARGET").unwrap();
+
+    if target.contains("android") {
+        let ndk_home = env::var("ANDROID_NDK_HOME").unwrap();
+        let abi = get_abi(target.clone());
+        cmake::Config::new(&ls_qpack_dep_dir)
+            .define(
+                "CMAKE_TOOLCHAIN_FILE",
+                format!("{}/build/cmake/android.toolchain.cmake", ndk_home),
+            )
+            .define("LSQPACK_BIN", "OFF")
+            .define("ANDROID_ABI", abi)
+            .build();
+    } else {
+        cmake::Config::new(&ls_qpack_dep_dir)
+            .define("LSQPACK_BIN", "OFF")
+            .build();
+    }
 
     println!(
         "cargo:rustc-link-search=native={}",
@@ -17,28 +46,64 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=ls-qpack");
 
-    let builder = bindgen::Builder::default()
-        .derive_debug(true)
-        .derive_default(true)
-        .derive_copy(false)
-        .size_t_is_usize(true)
-        .layout_tests(true)
-        .generate_comments(false)
-        .clang_arg(format!("-I{}", out_dir.join("include").display()))
-        .header(
-            ls_qpack_dep_dir
-                .join("lsqpack.h")
-                .into_os_string()
-                .into_string()
-                .unwrap(),
-        )
-        .header(
-            ls_qpack_dep_dir
-                .join("lsxpack_header.h")
-                .into_os_string()
-                .into_string()
-                .unwrap(),
-        );
+    let builder;
+
+    if target.contains("android") {
+        let ndk_home = env::var("ANDROID_NDK_HOME").unwrap();
+        builder = bindgen::Builder::default()
+            .derive_debug(true)
+            .derive_default(true)
+            .derive_copy(false)
+            .size_t_is_usize(true)
+            .layout_tests(true)
+            .generate_comments(false)
+            .clang_arg(format!("-I{}", out_dir.join("include").display()))
+            .clang_arg(format!(
+                "-I{}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include",
+                ndk_home
+            ))
+            .clang_arg(format!(
+                "-I{}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/{}",
+                ndk_home, target
+            ))
+            .header(
+                ls_qpack_dep_dir
+                    .join("lsqpack.h")
+                    .into_os_string()
+                    .into_string()
+                    .unwrap(),
+            )
+            .header(
+                ls_qpack_dep_dir
+                    .join("lsxpack_header.h")
+                    .into_os_string()
+                    .into_string()
+                    .unwrap(),
+            );
+    } else {
+        builder = bindgen::Builder::default()
+            .derive_debug(true)
+            .derive_default(true)
+            .derive_copy(false)
+            .size_t_is_usize(true)
+            .layout_tests(true)
+            .generate_comments(false)
+            .clang_arg(format!("-I{}", out_dir.join("include").display()))
+            .header(
+                ls_qpack_dep_dir
+                    .join("lsqpack.h")
+                    .into_os_string()
+                    .into_string()
+                    .unwrap(),
+            )
+            .header(
+                ls_qpack_dep_dir
+                    .join("lsxpack_header.h")
+                    .into_os_string()
+                    .into_string()
+                    .unwrap(),
+            );
+    }
 
     let bindings = builder.generate().expect("Unable to generate bindings");
     bindings
